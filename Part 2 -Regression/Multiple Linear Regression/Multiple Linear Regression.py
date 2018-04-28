6498934e9069e36a9e1d7209eb9d3da27522299e
akky2892 <36126567+akky2892@users.noreply.github.com>
2018-04-28 14:30:47 +0530

    Add files via upload

---
+++ Part 2 -Regression/Multiple Linear Regression/Multiple Linear Regression.py
@@ -0,0 +1,64 @@
+
+"""
+Multiple Linear Regression - Backward Elimination
+"""
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#Importing the dataset
+dataset = pd.read_csv('Data_company.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:,4].values
+print (X)
+print (y)
+
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding dummy variable trap
+X = X[:, 1:]
+
+
+#Splitting into training and test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
+
+
+#Fitting multiple linear regression to trainingset
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting the test set results
+y_pred = regressor.predict(X_test)
+
+#Building the optimla model using Backward elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0,2,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:, [0,3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
